d218056700c2f3a51c78189b0e060031f83667d3
Marina-Portillo <maryypucca@gmail.com>
2025-10-06 12:42:17 -0600

    mi Primer PWA

---
+++ sw.js
@@ -0,0 +1,62 @@
+
+//1. Nombgre del sw y los archivos a cachear
+
+const CACHE_NAME = "mi-cache";
+const urlsToCache = [
+    "index.htlm",
+    "manifest.json",
+    "style.css",
+    "app.js",
+    "offline.htlm",
+    "icons/icon-192x192.png",
+    "icons/icon-512x512.png",
+];
+
+//2. INSTALL -> se ejecuta al instalar el service worker
+// se cachean(se meten a cache) los recuros base de la PWA
+self.addEventListener("install", event => {
+    console.log("Service Worker: Instalando...");
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Archivos cacheados");
+            return cache.addAll(urlsToCache)
+})
+    );
+});
+ 
+// 3. ACTIVATE -> Se ejecuta al activarse el sw limpiar el cache viejo, 
+// para mantener solo la version actual de la cache
+
+self.addEventListener("activate", event => {
+        event.waitUntil(
+            caches.keys().then(keys =>
+                Promise.all(
+                    keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+                )
+            )
+        );
+});
+
+// 4. FETCH -> Intercepta peticiones de la app
+// Intercepta cada peticion de la PWA
+// Busca primero en cachè y si no esta, busca en Internet
+// En caso de falla, muestra la pagina offline.html.
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request).catch (() => caches.match("offline.html"))
+        })
+    )
+});
+
+// 5. PUSH -> notificaciones en segundo plano Manejo de notificaciones
+// push (opcional)
+
+self.addEventListener("push", event => {
+    const data = event.data ? event.data.text() : "Notificaciòn sin texto"
+    event.waitUntil(
+        self.ServiceWorkerRegistration.showNotificacion("Mi PWA", {body:data})
+    );
+});
+
